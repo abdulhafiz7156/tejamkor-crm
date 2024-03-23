d9864c08b46700131ba8ef1ac5e8b95afb0a9f5e
abdulhafiz7156 <xxafiz69@gmail.com>
2024-03-23 22:35:45 +0500

    for fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  await app.listen(port);
+  console.log(`Server running on port http://localhost:${port}/`)
+  console.log(this.PrismaService.user)
 }
 bootstrap();
---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,6 @@
+export class UserEntity {
+    id: number;
+    username: string;
+    password: string;
+    role_id: number;
+}
